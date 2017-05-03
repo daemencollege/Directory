ac604aeb9ec5c0d3c36c461baaa8806813d18442
Alex Herdzik <aherdzik@daemen.edu>
2017-05-03 09:58:58 -0400

    Assign Departments

---
+++ random-assign.php
@@ -0,0 +1,20 @@
+<?php
+require 'connect.php';
+
+function gen_department(){
+	$department = array(1,2,3,4,5,6);
+	$department = $department[array_rand($department)];
+	return $department;
+}
+
+$link = db_connect();
+
+for($i = 1; $i <= 200; $i++) {
+	
+	$department = gen_department();
+	
+	$insert = "INSERT INTO in_department (person_id, department_id) VALUES ($i, $department)";
+	
+	mysqli_query($link, $insert) or die(mysqli_error($link));
+}
+	
